1b05822c2cdedab9f99a8b17495b5eba03a34e4e
David <arts.engineering@gmail.com>
2015-08-20 17:15:38 -0400

    new class

---
+++ mbdriver.cpp
@@ -0,0 +1,5 @@
+#include "mbdriver.h"
+
+MBDriver::MBDriver()
+{
+}
---
+++ mbdriver.h
@@ -0,0 +1,10 @@
+#ifndef MBDRIVER_H
+#define MBDRIVER_H
+
+class MBDriver
+{
+public:
+    MBDriver();
+};
+
+#endif // MBDRIVER_H
